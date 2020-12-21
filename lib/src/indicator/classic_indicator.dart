1545ebcab46f352238f9921997c540a10e2442a0
Leon Alonso <65343272+leonalonso@users.noreply.github.com>
2020-12-21 03:34:05 +0100

    Update classic_indicator.dart

--- lib/src/indicator/classic_indicator.dart
+++ lib/src/indicator/classic_indicator.dart
@@ -61,7 +61,7 @@ class ClassicHeader extends RefreshIndicator {
     Key key,
     RefreshStyle refreshStyle: RefreshStyle.Follow,
     double height: 60.0,
-    double padding: 60.0,
+    double padding: EdgeInsets.only(top: 2, bottom: 2),
     Duration completeDuration: const Duration(milliseconds: 600),
     this.outerBuilder,
     this.textStyle: const TextStyle(color: Colors.grey),
